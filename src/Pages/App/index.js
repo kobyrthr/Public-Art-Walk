d7cb5ba0d10449eba2ebc4f2ec7fa2f6ccd6af48
kobyrthr-dev <kobyrthr@gmail.com>
2023-05-31 19:37:19 -0400

    added pitch

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -73,7 +73,8 @@ const MapApp =()=> {
           initialViewState={{
             longitude: -76.6122,
             latitude: 39.2904,
-            zoom: 12,
+            zoom: 12.5,
+            pitch:60
           }}
           style={{height: '100vh'}}
           mapStyle="mapbox://styles/mapbox/light-v10"
