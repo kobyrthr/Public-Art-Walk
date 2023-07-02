5503f49e8f3ac617cea334ea06f5792fbec88e61
kobyrthr-dev <kobyrthr@gmail.com>
2023-07-02 19:09:45 -0400

    added mobile styling for the location list component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'mapbox-gl/dist/mapbox-gl.css';
 // import './normalize.css';
 // import './skeleton.css';
-// import './index.css';
+import './index.css';
 import LandingPage from './Pages/LandingPage';
 import MapPage from './Pages/MapPage';
 import SubmitPage from './Pages/Submit page';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 const client = new ApolloClient({
   uri: 'https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/clgyincet1u6l01um0ig492sn/master',
