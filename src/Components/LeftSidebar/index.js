49e6657ad648e61459bcc45ee23251aceae63593
kobyrthr-dev <kobyrthr@gmail.com>
2023-07-02 20:55:09 -0400

    added chakra component

--- src/Components/LeftSidebar/index.js
+++ src/Components/LeftSidebar/index.js
@@ -1,26 +1,76 @@
 import React, { useState } from 'react';
 import styles from './LeftSidebar.module.css'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input,
+  useDisclosure,
+  Button
+} from '@chakra-ui/react'
+
 
 export default function LeftSidebar({pins}) {
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+
   return (
-    <div className= {styles.sidebar}>
-        <div className='heading'>
-          <h1>Locations</h1>
-        </div>
-        <br></br>
-       <div className={styles.locList}>
-         {pins.map((pin,index)=>{
-            return <li className={styles.locListItem} key={index}>
+    // CUSTOM COMPONENT
+    // <div className= {styles.sidebar}>
+    //     <div className='heading'>
+    //       <h1>Locations</h1>
+    //     </div>
+    //     <br></br>
+    //    <div className={styles.locList}>
+    //      {pins.map((pin,index)=>{
+    //         return <li className={styles.locListItem} key={index}>
 
-            <span>{pin.street}, {pin.postalCode}</span><br></br>
-            Artist: {pin.artist}<br></br>
-            {/* Year: {pin.year}<br></br> */}
+    //         <span>{pin.street}, {pin.postalCode}</span><br></br>
+    //         Artist: {pin.artist}<br></br>
+    //         {/* Year: {pin.year}<br></br> */}
 
-            </li>
-         })}
-       </div>
-        <div id={styles.listings} className={styles.listings}></div>
-    </div>
+    //         </li>
+    //      })}
+    //    </div>
+    //     <div id={styles.listings} className={styles.listings}></div>
+    // </div>
+
+    // CHAKRA COMPONENT
+    <>
+    <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+    </Button>
+    <Drawer
+    isOpen={isOpen}
+    placement='bottom'
+    onClose={onClose}
+    finalFocusRef={btnRef}
+  >
+    <DrawerOverlay />
+    <DrawerContent>
+      <DrawerCloseButton />
+      <DrawerHeader>Create your account</DrawerHeader>
+
+      <DrawerBody>
+        <Input placeholder='Type here...' />
+      </DrawerBody>
+
+      <DrawerFooter>
+        <Button variant='outline' mr={3} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button colorScheme='blue'>Save</Button>
+      </DrawerFooter>
+    </DrawerContent>
+  </Drawer>
+    
+    </>
+    
   )
 }
