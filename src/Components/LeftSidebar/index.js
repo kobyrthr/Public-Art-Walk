bb906c410190a5215a8c70171c1c4681f1f318d0
kobyrthr-dev <kobyrthr@gmail.com>
2023-07-04 14:38:19 -0400

    added locations to the drawer component

--- src/Components/LeftSidebar/index.js
+++ src/Components/LeftSidebar/index.js
@@ -9,40 +9,45 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Input,
+  Box, 
+  Center, 
+  Stack, 
+  StackDivider, 
+  Text,
   useDisclosure,
   Button
 } from '@chakra-ui/react'
-
-
 export default function LeftSidebar({pins}) {
 
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = React.useRef()
 
-
   return (
-    // CUSTOM COMPONENT
-    // <div className= {styles.sidebar}>
-    //     <div className='heading'>
-    //       <h1>Locations</h1>
-    //     </div>
-    //     <br></br>
-    //    <div className={styles.locList}>
-    //      {pins.map((pin,index)=>{
-    //         return <li className={styles.locListItem} key={index}>
-
-    //         <span>{pin.street}, {pin.postalCode}</span><br></br>
-    //         Artist: {pin.artist}<br></br>
-    //         {/* Year: {pin.year}<br></br> */}
-
-    //         </li>
-    //      })}
-    //    </div>
-    //     <div id={styles.listings} className={styles.listings}></div>
-    // </div>
-
-    // CHAKRA COMPONENT
     <>
+    {/* // CUSTOM COMPONENT */}
+    <div className= {styles.sidebar}>
+        <div className='heading'>
+          <h1>Locations</h1>
+        </div>
+        <br></br>
+       <div className={styles.locList}>
+         {pins.map((pin,index)=>{
+            return <li className={styles.locListItem} key={index}>
+
+            <span>{pin.street}, {pin.postalCode}</span><br></br>
+            Artist: {pin.artist}<br></br>
+            {/* Year: {pin.year}<br></br> */}
+
+            </li>
+         })}
+       </div>
+        <div id={styles.listings} className={styles.listings}></div>
+    </div>
+    
+
+
+    {/* // CHAKRA COMPONENT */}
+    <div>
     <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
         Open
     </Button>
@@ -51,14 +56,31 @@ export default function LeftSidebar({pins}) {
     placement='bottom'
     onClose={onClose}
     finalFocusRef={btnRef}
-  >
+    >
     <DrawerOverlay />
     <DrawerContent>
       <DrawerCloseButton />
-      <DrawerHeader>Create your account</DrawerHeader>
+      <DrawerHeader>Locations</DrawerHeader>
 
       <DrawerBody>
-        <Input placeholder='Type here...' />
+      <div className= {styles.mobileSidebar}>
+        {/* <div className='heading'>
+          <h1>Locations</h1>
+        </div> */}
+        <br></br>
+       <div className={styles.locList}>
+         {pins.map((pin,index)=>{
+            return <li className={styles.locListItem} key={index}>
+
+            <span>{pin.street}, {pin.postalCode}</span><br></br>
+            Artist: {pin.artist}<br></br>
+            {/* Year: {pin.year}<br></br> */}
+
+            </li>
+         })}
+       </div>
+        <div id={styles.listings} className={styles.listings}></div>
+    </div>
       </DrawerBody>
 
       <DrawerFooter>
@@ -69,6 +91,10 @@ export default function LeftSidebar({pins}) {
       </DrawerFooter>
     </DrawerContent>
   </Drawer>
+
+    </div>
+
+    
     
     </>
     
