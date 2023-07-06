b2e45c02e83898124e39a1ab8b50903059e745c9
kobyrthr-dev <kobyrthr@gmail.com>
2023-07-06 08:34:24 -0400

    added chakra components

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { MobileDrawer } from './MobileDrawer/MobileDrawer'
+import { PopoverIcon } from './PopOverIcon/PopOverIcon'
+import { DocumentCollapse } from '.DocumentCollapse/DocumentCollapse'
 
 function NavBar() {
   return (
     <div className='navbar columns twelve'>
-
-        <h1> Public Art Walk</h1>
+        <div>
+          <MobileDrawer></MobileDrawer>
+          <PopoverIcon></PopoverIcon>
+          <DocumentCollapse></DocumentCollapse>
+        </div>
+        <h1> Public Art Walk</h1> 
         <a href="/"><li>Map</li></a>
         <a href="/suggestions"><li>Suggestions</li></a>
     </div>
